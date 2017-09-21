f29d0c1edbf8788f3405b656ab156f0e3e7aa578
Matt Henderson <matt_henderson@sil.org>
2017-09-21 13:44:13 -0400

    Better handle invalid AES key length.

--- helpers/encryption.js
+++ helpers/encryption.js
@@ -25,6 +25,14 @@ module.exports.decrypt = (ivAndEncryptedDataBase64, aesKeyBase64, callback) => {
   const ivBuffer = Buffer.from(inputPieces[0], 'base64');
   const encryptedDataBase64 = inputPieces[1];
   const aesKeyBuffer = Buffer.from(aesKeyBase64, 'base64');
+  if (aesKeyBuffer.length !== 32) {
+    console.error(
+      'The encryption key must be 256 bits (32 Bytes). Found %s Bytes.',
+      aesKeyBuffer.length
+    );
+    callback(new Error('That is not a valid encryption key.'));
+    return;
+  }
   
   const decipher = crypto.createDecipheriv('aes-256-ctr', aesKeyBuffer, ivBuffer);
   let decrypted = decipher.update(encryptedDataBase64, 'base64', 'utf8');
