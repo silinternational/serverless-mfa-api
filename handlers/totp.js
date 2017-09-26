90c051c9ecdd7d862f5a833049f6a12ce90c5d36
Matt Henderson <matt_henderson@sil.org>
2017-09-26 14:44:51 -0400

    Remove deprecated comment.

--- handlers/totp.js
+++ handlers/totp.js
@@ -3,13 +3,6 @@
 const requestHelper = require('../helpers/request-helper.js');
 const totp = require('../models/totp.js');
 
-/*
- * NOTE: These handlers assume that any errors passed to their callbacks will
- *       be intended for the API caller. Any error messages with potentially
- *       sensitive data should be handled elsewhere, and only safe error
- *       messages should be passed to callbacks.
- */
- 
 module.exports.create = (event, context, callback) => {
   const {apikey = '', apisecret = ''} = requestHelper.getTotpHeaders(event.headers);
   const options = requestHelper.getJsonData(event.body || undefined);
