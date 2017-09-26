4f61aa38f7771d481f96888156692080fa9158d9
Matt Henderson <matt_henderson@sil.org>
2017-09-26 15:06:04 -0400

    Fix `apiKey` variable naming collision.

--- handlers/api-key.js
+++ handlers/api-key.js
@@ -4,8 +4,8 @@ const apiKey = require('../models/api-key.js');
 const requestHelper = require('../helpers/request-helper.js');
 
 module.exports.activate = (event, context, callback) => {
-  const {apiKey = '', email = ''} = requestHelper.getJsonData(event.body);
-  apiKey.activate(apiKey, email, callback);
+  const {apiKey:apiKeyValue = '', email = ''} = requestHelper.getJsonData(event.body);
+  apiKey.activate(apiKeyValue, email, callback);
 };
 
 module.exports.create = (event, context, callback) => {
