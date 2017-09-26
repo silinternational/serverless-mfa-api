f3a09cb6ff8b9c5994f481ba5c0588d112e46a58
Matt Henderson <matt_henderson@sil.org>
2017-09-26 14:59:43 -0400

    Move JSON body parsing out of apiKey.activate().

--- handlers/api-key.js
+++ handlers/api-key.js
@@ -4,7 +4,8 @@ const apiKey = require('../models/api-key.js');
 const requestHelper = require('../helpers/request-helper.js');
 
 module.exports.activate = (event, context, callback) => {
-  apiKey.activate(event.body, callback);
+  const {apiKey = '', email = ''} = requestHelper.getJsonData(event.body);
+  apiKey.activate(apiKey, email, callback);
 };
 
 module.exports.create = (event, context, callback) => {
