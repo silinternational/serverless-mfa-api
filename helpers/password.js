b28a45d41fe4baa1fde541e207f8cecae1a2d275
Matt Henderson <matt_henderson@sil.org>
2017-09-27 17:07:40 -0400

    Add warning about risk of timing attacks.

--- helpers/password.js
+++ helpers/password.js
@@ -1,5 +1,10 @@
 const bcrypt = require('bcrypt');
 
+/**
+ * WARNING: This requires strings, and if the value of hashedPassword is not an
+ *          actual hash, this will return almost instantly, which may expose
+ *          the existence of things through timing attacks.
+ */
 module.exports.compare = (givenPassword, hashedPassword) => {
   return bcrypt.compareSync(givenPassword, hashedPassword);
 };
