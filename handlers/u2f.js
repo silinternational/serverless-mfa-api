7ab3fa6bdec8229fa6e346fcaa7f1f9b2af3360b
Phillip Shipley <phillip.shipley@gmail.com>
2017-10-12 10:17:46 -0400

    catch unhandled exceptions from models

--- handlers/u2f.js
+++ handlers/u2f.js
@@ -2,35 +2,56 @@
 
 const requestHelper = require('../helpers/request-helper.js');
 const u2f = require('../models/u2f.js');
+const response = require('../helpers/response.js');
 
 module.exports.createAuthentication = (event, context, callback) => {
-  const {apikey = '', apisecret = ''} = requestHelper.getMfaHeaders(event.headers);
-  const {uuid = ''} = event.pathParameters;
-  u2f.createAuthentication(apikey, apisecret, uuid, callback);
+  try {
+    const {apikey = '', apisecret = ''} = requestHelper.getMfaHeaders(event.headers);
+    const {uuid = ''} = event.pathParameters;
+    u2f.createAuthentication(apikey, apisecret, uuid, callback);
+  } catch (e) {
+    response.returnError(500, e.message, callback)
+  }
 };
 
 module.exports.createRegistration = (event, context, callback) => {
-  const {apikey = '', apisecret = ''} = requestHelper.getMfaHeaders(event.headers);
-  const options = requestHelper.getJsonData(event.body);
-  u2f.createRegistration(apikey, apisecret, options, callback);
+  try {
+    const {apikey = '', apisecret = ''} = requestHelper.getMfaHeaders(event.headers);
+    const options = requestHelper.getJsonData(event.body);
+    u2f.createRegistration(apikey, apisecret, options, callback);
+  } catch (e) {
+    response.returnError(500, e.message, callback)
+  }
 };
 
 module.exports.delete = (event, context, callback) => {
-  const {apikey = '', apisecret = ''} = requestHelper.getMfaHeaders(event.headers);
-  const {uuid = ''} = event.pathParameters;
-  u2f.delete(apikey, apisecret, uuid, callback);
+  try {
+    const {apikey = '', apisecret = ''} = requestHelper.getMfaHeaders(event.headers);
+    const {uuid = ''} = event.pathParameters;
+    u2f.delete(apikey, apisecret, uuid, callback);
+  } catch (e) {
+    response.returnError(500, e.message, callback)
+  }
 };
 
 module.exports.validateAuthentication = (event, context, callback) => {
-  const {apikey = '', apisecret = ''} = requestHelper.getMfaHeaders(event.headers);
-  const {uuid = ''} = event.pathParameters;
-  const options = requestHelper.getJsonData(event.body);
-  u2f.validateAuthentication(apikey, apisecret, uuid, options, callback);
+  try {
+    const {apikey = '', apisecret = ''} = requestHelper.getMfaHeaders(event.headers);
+    const {uuid = ''} = event.pathParameters;
+    const options = requestHelper.getJsonData(event.body);
+    u2f.validateAuthentication(apikey, apisecret, uuid, options, callback);
+  } catch (e) {
+    response.returnError(500, e.message, callback)
+  }
 };
 
 module.exports.validateRegistration = (event, context, callback) => {
-  const {apikey = '', apisecret = ''} = requestHelper.getMfaHeaders(event.headers);
-  const {uuid = ''} = event.pathParameters;
-  const options = requestHelper.getJsonData(event.body);
-  u2f.validateRegistration(apikey, apisecret, uuid, options, callback);
+  try {
+    const {apikey = '', apisecret = ''} = requestHelper.getMfaHeaders(event.headers);
+    const {uuid = ''} = event.pathParameters;
+    const options = requestHelper.getJsonData(event.body);
+    u2f.validateRegistration(apikey, apisecret, uuid, options, callback);
+  } catch (e) {
+    response.returnError(500, e.message, callback)
+  }
 };
