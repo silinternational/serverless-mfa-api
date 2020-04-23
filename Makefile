932dfb7543110025b661ce9d6ae1c6a9554112de
Matt Henderson <matt_henderson@sil.org>
2020-04-23 17:58:18 -0400

    Begin adding local dev-server to proxy calls to our handler methods

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 start:
 	echo This Makefile has no default action. Be specific.
 
+dev-server:
+	node development/server
+
 do-full-recovery:
 	docker-compose run --rm do-full-recovery
 
---
+++ development/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const u2fHandlers = require('../handlers/u2f.js');
+
+const app = express();
+
+app.post('/u2f', ({headers, body}, response) => {
+  u2fHandlers.createRegistration({headers, body}, {}, (error, data) => {
+    if (error) {
+      response.status(400).send(error);
+    } else {
+      response.status(data.statusCode).send(data.body);
+    }
+  })
+});
+
+app.listen(8000, () => {
+  console.log('Local development server listening on port 8000')
+});
