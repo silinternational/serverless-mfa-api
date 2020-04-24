dab7d8236479dffbffe377ba1372c1968cc1d78e
Matt Henderson <matt_henderson@sil.org>
2020-04-24 13:44:57 -0400

    Further extract duplications to make url-mappings simpler

--- development/server.js
+++ development/server.js
@@ -2,33 +2,34 @@ const express = require('express');
 const u2fHandlers = require('../handlers/u2f.js');
 
 const app = express();
-
+const mapTo = requestHandler => (request, response) => {
+  const {event, callback} = mimicApiGateway(request, response);
+  requestHandler(event, {}, callback);
+};
 const mimicApiGateway = (request, response) => {
   return {
-    event: {
-      headers: request.headers,
-      body: request.body,
-      pathParameters: request.params,
-    },
-    callback: (error, data) => {
-      if (error) {
-        response.status(500).send(error);
-      } else {
-        response.status(data.statusCode).send(data.body);
-      }
-    }
+    event: createEventFrom(request),
+    callback: createCallbackUsing(response),
   };
 };
+const createEventFrom = request => {
+  return {
+    headers: request.headers,
+    body: request.body,
+    pathParameters: request.params,
+  }
+};
+const createCallbackUsing = response => (error, data) => {
+  if (error) {
+    response.status(500).send(error);
+  } else {
+    response.status(data.statusCode).send(data.body);
+  }
+};
 
-app.post('/u2f', (request, response) => {
-  const {event, callback} = mimicApiGateway(request, response);
-  u2fHandlers.createRegistration(event, {}, callback);
-});
-
-app.put('/u2f/:uuid', (request, response) => {
-  const {event, callback} = mimicApiGateway(request, response);
-  u2fHandlers.validateRegistration(event, {}, callback);
-});
+// Define URL mapping.
+app.post('/u2f', mapTo(u2fHandlers.createRegistration));
+app.put('/u2f/:uuid', mapTo(u2fHandlers.validateRegistration));
 
 app.listen(8000, () => {
   console.log('Local development server listening on port 8000')
