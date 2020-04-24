704d2e79b26b1a708164dfcedd8feadac92c560f
Matt Henderson <matt_henderson@sil.org>
2020-04-24 13:31:50 -0400

    Handle calls to `/u2f/:uuid` and reduce duplication

--- development/server.js
+++ development/server.js
@@ -3,14 +3,31 @@ const u2fHandlers = require('../handlers/u2f.js');
 
 const app = express();
 
-app.post('/u2f', ({headers, body}, response) => {
-  u2fHandlers.createRegistration({headers, body}, {}, (error, data) => {
-    if (error) {
-      response.status(500).send(error);
-    } else {
-      response.status(data.statusCode).send(data.body);
+const mimicApiGateway = (request, response) => {
+  return {
+    event: {
+      headers: request.headers,
+      body: request.body,
+      pathParameters: request.params,
+    },
+    callback: (error, data) => {
+      if (error) {
+        response.status(500).send(error);
+      } else {
+        response.status(data.statusCode).send(data.body);
+      }
     }
-  })
+  };
+};
+
+app.post('/u2f', (request, response) => {
+  const {event, callback} = mimicApiGateway(request, response);
+  u2fHandlers.createRegistration(event, {}, callback);
+});
+
+app.put('/u2f/:uuid', (request, response) => {
+  const {event, callback} = mimicApiGateway(request, response);
+  u2fHandlers.validateRegistration(event, {}, callback);
 });
 
 app.listen(8000, () => {
