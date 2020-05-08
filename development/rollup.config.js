5a8cfe3a5c58c15d9b9455c4842bff3dbb7c6cc6
Matt Henderson <matt_henderson@sil.org>
2020-05-08 11:59:19 -0400

    Remove (now unused) commonjs and node-resolve rollup plugins

--- development/rollup.config.js
+++ development/rollup.config.js
@@ -1,14 +1,7 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-
 export default {
   input: 'development/site/client.js',
   output: {
     file: 'development/site/client-bundle.js',
     format: 'iife'
-  },
-  plugins: [
-    resolve(),
-    commonjs()
-  ]
+  }
 };
