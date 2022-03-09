bc1e78baede59ff284df8c8ef0fc1769a1d9676c
Schparky <3172830+Schparky@users.noreply.github.com>
2022-03-09 16:28:47 -0700

    use variable names consistent with IDP AWS account

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,8 +2,8 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "aws_access_key" {
+variable "aws_access_key_id" {
 }
 
-variable "aws_secret_key" {
+variable "aws_secret_access_key" {
 }
