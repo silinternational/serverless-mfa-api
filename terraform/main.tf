f6822ec06896b92eb1b018c8bbb5fa7a9506fa66
Schparky <3172830+Schparky@users.noreply.github.com>
2022-03-17 14:44:24 -0600

    use module version 0.1.1 with included GetBucketPolicy permission

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,13 +4,11 @@
  */
 module "serverless-user" {
   source  = "silinternational/serverless-user/aws"
-  version = "0.1.0"
+  version = "0.1.1"
 
   app_name           = "mfa-api"
   aws_region         = var.aws_region
   enable_api_gateway = true
-
-  extra_policies = [local.s3_policy]
 }
 
 output "serverless-access-key-id" {
@@ -19,22 +17,4 @@ output "serverless-access-key-id" {
 output "serverless-secret-access-key" {
   value     = module.serverless-user.aws_secret_access_key
   sensitive = true
-}
-
-
-locals {
-  s3_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "s3:GetBucketPolicy",
-        ],
-        "Resource" : [
-          "arn:aws:s3:::mfa-api-*-serverlessdeploymentbucket*",
-        ]
-      },
-    ]
-  })
 }
