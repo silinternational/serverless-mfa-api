f9764a2240c2e1b13831c1e90e11147af8204679
Matt H <matt_henderson@sil.org>
2022-09-21 15:46:13 -0400

    Use NodeJS 16 in our CI/CD processes

--- codeship/setup.sh
+++ codeship/setup.sh
@@ -3,5 +3,9 @@
 # Exit script with error if any step fails.
 set -e
 
+# Specify the desired version of NodeJS. Should match package.json, Dockerfiles,
+# and serverless.yml file.
+nvm use 16
+
 npm install --no-fund -g serverless@3
 npm install --no-fund
