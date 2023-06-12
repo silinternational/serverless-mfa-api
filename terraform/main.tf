19cf0c9c70656cd8a4fceb15936a2839ba9d9674
Matt H <matt_henderson@sil.org>
2023-06-12 16:06:53 -0400

    Add terraform resources for DynamoDB (global) tables, plus new variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,3 +68,71 @@ module "serverless-user" {
     )
   ]
 }
+
+
+/*
+ * Manage DynamoDB tables used by the functions.
+ */
+
+resource "aws_dynamodb_table" "api_keys" {
+  name             = "mfa-api_${var.app_env}_api-key_global"
+  hash_key         = "value"
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+
+  attribute {
+    name = "value"
+    type = "S"
+  }
+
+  replica {
+    region_name = var.aws_region_secondary
+  }
+
+  lifecycle {
+    ignore_changes = [replica]
+  }
+}
+
+resource "aws_dynamodb_table" "totp" {
+  name             = "mfa-api_${var.app_env}_totp_global"
+  hash_key         = "uuid"
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+
+  attribute {
+    name = "uuid"
+    type = "S"
+  }
+
+  replica {
+    region_name = var.aws_region_secondary
+  }
+
+  lifecycle {
+    ignore_changes = [replica]
+  }
+}
+
+resource "aws_dynamodb_table" "u2f" {
+  name             = "mfa-api_${var.app_env}_u2f_global"
+  hash_key         = "uuid"
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+
+  attribute {
+    name = "uuid"
+    type = "S"
+  }
+
+  replica {
+    region_name = var.aws_region_secondary
+  }
+
+  lifecycle {
+    ignore_changes = [replica]
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,10 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
+variable "aws_region_secondary" {
+  default = "us-west-2"
+}
+
 variable "aws_access_key_id" {
 }
 
@@ -16,6 +20,12 @@ variable "app_customer" {
   description = "customer name to use for the itse_app_customer tag"
   type        = string
   default     = "shared"
+}
+
+variable "app_env" {
+  description = "Short environment name (e.g. 'dev' or 'prod'), used in some resource names"
+  type        = string
+  default     = "prod"
 }
 
 variable "app_environment" {
