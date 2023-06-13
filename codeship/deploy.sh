459178c9b560679f584e3cfef724d39f22bf5d3e
Matt H <matt_henderson@sil.org>
2023-06-13 16:55:17 -0400

    Add comment about multiple places the region(s) is/are specified

--- codeship/deploy.sh
+++ codeship/deploy.sh
@@ -9,6 +9,10 @@ set -x
 # Print the Serverless version in the logs
 serverless --version
 
+# NOTE: The regions are hard-coded here (to specify where the AWS Lambda
+# functions should be deployed to), but are provided via variables in terraform
+# (to control where the DynamoDB GlobalTables exist). If you want them to be in
+# the same regions, set the terraform variables to match these:
 echo "Deploying stage $1..."
 serverless deploy --verbose --stage "$1" --region us-east-1
 serverless deploy --verbose --stage "$1" --region us-west-2
