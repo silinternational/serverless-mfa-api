f1175edc2f927616ea80c5355614f3d95ad3efed
Matt H <matt_henderson@sil.org>
2023-06-15 14:27:37 -0400

    Alphabetize variables in acm-certificate local module

--- terraform/aws/acm-certificate/variables.tf
+++ terraform/aws/acm-certificate/variables.tf
@@ -1,9 +1,4 @@
 
-variable "create_dns_validation" {
-  description = "Whether to create the DNS record for certificate validation"
-  type        = bool
-}
-
 variable "certificate_domain_name" {
   description = "Exact domain name for the certificate"
   type        = string
@@ -12,4 +7,9 @@ variable "certificate_domain_name" {
 variable "cloudflare_zone_name" {
   description = "The Cloudflare zone (aka domain) name for the DNS record for certificate validation."
   type        = string
+}
+
+variable "create_dns_validation" {
+  description = "Whether to create the DNS record for certificate validation"
+  type        = bool
 }
