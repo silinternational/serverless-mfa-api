ee77cb581e4925be62059ca8841655562290a2c3
Matt H <matt_henderson@sil.org>
2023-06-15 10:59:44 -0400

    Set up 2nd AWS provider (for secondary region)

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,15 +1,31 @@
+locals {
+  tags = {
+    managed_by        = "terraform"
+    workspace         = terraform.workspace
+    itse_app_customer = var.app_customer
+    itse_app_env      = var.app_environment
+    itse_app_name     = var.app_name
+  }
+}
+
 provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 
   default_tags {
-    tags = {
-      managed_by        = "terraform"
-      workspace         = terraform.workspace
-      itse_app_customer = var.app_customer
-      itse_app_env      = var.app_environment
-      itse_app_name     = var.app_name
-    }
+    tags = local.tags
   }
+}
+
+provider "aws" {
+  region     = var.aws_region_secondary
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+
+  default_tags {
+    tags = local.tags
+  }
+
+  alias = "secondary"
 }
