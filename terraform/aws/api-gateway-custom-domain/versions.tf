ffee3d25c1277a542428d119971ea5369eabd26e
Matt H <matt_henderson@sil.org>
2023-06-19 13:33:19 -0400

    Add `versions` files to local modules
    This is recommended by the Terraform documentation, which also suggests
    only specifying minimum provider versions (not max versions) in modules:
    https://developer.hashicorp.com/terraform/language/modules/develop/providers#provider-aliases-within-modules

---
+++ terraform/aws/api-gateway-custom-domain/versions.tf
@@ -0,0 +1,14 @@
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = ">= 2"
+    }
+  }
+}
