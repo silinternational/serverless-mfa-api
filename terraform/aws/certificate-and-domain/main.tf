c7f7e1246890a5a7f7f5a98ecd5bc399d2cd4941
Matt H <matt_henderson@sil.org>
2023-06-19 13:39:56 -0400

    For now, use direct URL-based source for our new acm-certificate module

--- terraform/aws/certificate-and-domain/main.tf
+++ terraform/aws/certificate-and-domain/main.tf
@@ -4,8 +4,7 @@ locals {
 }
 
 module "certificate" {
-  source  = "silinternational/acm-certificate/aws"
-  version = "0.1.0"
+  source = "github.com/silinternational/terraform-aws-acm-certificate?ref=0.1.0"
 
   certificate_domain_name = local.full_domain_name
   cloudflare_zone_name    = var.cloudflare_zone_name
