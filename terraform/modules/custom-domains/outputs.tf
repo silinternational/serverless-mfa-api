fed4880a8772aab5e56a8d35e45dce6cfe21c46e
Matt H <matt_henderson@sil.org>
2023-06-22 16:08:59 -0400

    Use external `terraform-aws-serverless-api-dns-for-failover` module

--- terraform/modules/custom-domains/outputs.tf
+++
@@ -1,8 +0,0 @@
-
-output "primary_region_domain_name" {
-  value = module.primary-region.regional_domain_name
-}
-
-output "secondary_region_domain_name" {
-  value = module.secondary-region.regional_domain_name
-}
