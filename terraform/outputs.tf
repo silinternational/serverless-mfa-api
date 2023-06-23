37ccbe7df6b7cabdb1021dee04d2ae7784c623a9
Matt H <matt_henderson@sil.org>
2023-06-23 12:56:44 -0400

    Rename outputs to use underscores, not hyphens

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 
-output "serverless-access-key-id" {
+output "serverless_access_key_id" {
   value = one(module.serverless_user[*].aws_access_key_id)
 }
 
-output "serverless-secret-access-key" {
+output "serverless_secret_access_key" {
   value     = one(module.serverless_user[*].aws_secret_access_key)
   sensitive = true
 }
